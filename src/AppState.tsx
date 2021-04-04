52c64db3ff9ceccda500c4461e4b34310f785130
king-coyote <alex.hartstone@gmail.com>
2021-04-04 17:42:30 +1000

    app state loaded from localStorage

--- src/AppState.tsx
+++ src/AppState.tsx
@@ -2,22 +2,26 @@ import React, { Dispatch } from 'react';
 import {AppState, AppReducer, Action} from './types/types';
 import {appstate_reducer} from './reducers';
 
-interface ProviderProps {
-    children: React.ReactNode;
-};
+const get_initial_state = () => {
+    const local_stored = localStorage.getItem('planet_state');
+    if (local_stored) {
+        return JSON.parse(local_stored);
+    }
+    return {
+        dispatch: (action: Action) => {},
+        currentlyDragging: '',
+        graphs: {},
+        nodes: {},
+        appStage: 'HOME',
+        currentGraph: '',
+    };
+}
 
-const initialState: AppState = {
-    dispatch: (action: Action) => {},
-    currentlyDragging: '',
-    graphs: {},
-    nodes: {},
-    appStage: 'HOME',
-    currentGraph: '',
-};
-const AppContext = React.createContext<AppState>(initialState);
+
+const AppContext = React.createContext<AppState>(get_initial_state());
 
 const StateProvider: React.FC = ( { children } ) => {
-    const [state, dispatch] = React.useReducer<AppReducer>(appstate_reducer, initialState);
+    const [state, dispatch] = React.useReducer<AppReducer>(appstate_reducer, get_initial_state());
   
     return (
         <AppContext.Provider 
