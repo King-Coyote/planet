9b38f2806e063e5a2cf974df51268ad0e53f1b92
king-coyote <alex.hartstone@gmail.com>
2021-05-03 09:27:35 +1000

    fixed bug with dragging

--- src/hooks/useDrag.tsx
+++ src/hooks/useDrag.tsx
@@ -3,28 +3,32 @@ import {Pos, MaybeElement, DEFAULT_POS} from '../types/types';
 
 interface DragState {
     is_dragging: boolean;
-    origin_pos: Pos;
+    origin_client_pos: Pos;
+    origin_element_pos: Pos;
 };
 type UseDragReturn = [Pos, (e: React.MouseEvent) => void, boolean];
 const useDrag = (transformable: MaybeElement): UseDragReturn => {
     const [pos, setPos] = React.useState<Pos>({x: 0, y: 0});
     const [state, setState] = React.useState<DragState>({
         is_dragging: false,
-        origin_pos: DEFAULT_POS,
+        origin_client_pos: DEFAULT_POS,
+        origin_element_pos: DEFAULT_POS,
     });
 
-    const {origin_pos, is_dragging} = state;
+    const {origin_client_pos, origin_element_pos, is_dragging} = state;
 
     const handleMouseDown = React.useCallback((e: React.MouseEvent) => {
         console.log('mouse down for drag...');
         if (!transformable) {
             return;
         }
-        console.log('... node found. Starting drag state');
+        console.log(`... node found. Starting drag state at ${e.clientX},${e.clientY}`);
+        const bb = transformable.getBoundingClientRect();
         setState({
             ...state,
             is_dragging: true,
-            origin_pos: {x: e.clientX, y: e.clientY},
+            origin_client_pos: {x: e.clientX, y: e.clientY},
+            origin_element_pos: {x: bb.left, y: bb.top},
         });
     }, [transformable]);
 
@@ -37,20 +41,20 @@ const useDrag = (transformable: MaybeElement): UseDragReturn => {
     };
 
     const handleMouseMove = React.useCallback((e: MouseEvent) => {
-        console.log(`starting drag move at ${state.origin_pos.x},${state.origin_pos.y}...`);
+        console.log(`starting drag move at ${state.origin_client_pos.x},${state.origin_client_pos.y}...`);
         if (!transformable) {
             return;
         }
         const delta = {
-            x:  e.clientX - origin_pos.x,
-            y:  e.clientY - origin_pos.y
+            x:  e.clientX - origin_client_pos.x,
+            y:  e.clientY - origin_client_pos.y
         };
         console.log(`... node found: dragging with delta ${delta.x},${delta.y}.`);
         setPos({
-            x: pos.x + delta.x,
-            y: pos.y + delta.y
+            x: origin_element_pos.x + delta.x,
+            y: origin_element_pos.y + delta.y
         });
-    }, [transformable]);
+    }, [transformable, state]);
 
     React.useEffect(() => {
         if (is_dragging) {
